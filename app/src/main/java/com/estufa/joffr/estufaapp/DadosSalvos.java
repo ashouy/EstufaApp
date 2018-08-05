49f71a030eaf91b9f9660f514dcfb6580331bf1c
joffr <joffreypag@gmail.com>
2018-08-05 18:49:52 -0300

    Implementação de envio para o firebase

--- app/src/main/java/com/estufa/joffr/estufaapp/DadosSalvos.java
+++ app/src/main/java/com/estufa/joffr/estufaapp/DadosSalvos.java
@@ -1,16 +1,31 @@
 package com.estufa.joffr.estufaapp;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -24,10 +39,18 @@ import java.util.List;
 
 public class DadosSalvos extends AppCompatActivity {
 
+    private ProgressBar progressoenvio;
+    private List<Umidade> dados;
+    private CoordinatorLayout tela;
+    private boolean connected = false;
+    private Button botaonuvem;
     private TextView tv;
     private ProgressDialog pd;
     //private static String url = "http://192.168.0.11/api-rest-php/view/Conteudo/listar.php";
     private static String url = "http://192.168.50.1:8080/Pomodoro/umidade";
+
+    //FIREBASE
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +61,22 @@ public class DadosSalvos extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        tela = findViewById(R.id.teladados);
+        botaonuvem = findViewById(R.id.botaoSalvarNuvem);
+
+        //teste se há conexão com o banco no momento
+        DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                connected = dataSnapshot.getValue(Boolean.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(DadosSalvos.this, "Listener de conexão perdida\n"+databaseError.getDetails(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FloatingActionButton fabjson = findViewById(R.id.fabjson);
         fabjson.setOnClickListener(new View.OnClickListener() {
@@ -46,8 +85,37 @@ public class DadosSalvos extends AppCompatActivity {
 
         //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX//
                 new JsonTask().execute(url);
-                // new GetContacts().execute();
         //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX//
+            }
+        });
+        //instanciando o banco da nuvem
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        progressoenvio = findViewById(R.id.progressBarnuvem);
+        botaonuvem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                if (connected){
+                //alert dialog de advertencia para o que o usuario esta fazendo
+                AlertDialog alerta = new AlertDialog.Builder(DadosSalvos.this)
+                        .setTitle("Enviar para nuvem")
+                        .setMessage("Deseja enviar os dados agora?")
+                        .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                enviarDados();
+                            }
+                        })
+                        .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Snackbar.make(tela, "Envio cancelado", Snackbar.LENGTH_SHORT).show();
+                            }
+                        }).create();
+                alerta.show();
+//                }else{
+//                    Snackbar.make(tela, "Incapaz de enviar os dados agora, verifique sua conexão", Snackbar.LENGTH_SHORT).show();
+//                }
             }
         });
 
@@ -57,15 +125,58 @@ public class DadosSalvos extends AppCompatActivity {
     }
 
     private void atualizarMsg(){
-        List<Umidade> dados = Umidade.listAll(Umidade.class);
+        dados = Umidade.listAll(Umidade.class);
 
         String text = String.valueOf(dados.size());
+        //tratamento para o botao de enviar dados para nuvem, se nao tiver dados a ser emviados, ele não aparece ao usuario
+        if (dados.size()>0) {
+            botaonuvem.setVisibility(View.VISIBLE);
+        }else{
+            botaonuvem.setVisibility(View.GONE);
+        }
 
+        tv.setText(text);
 //        for (Umidade u : dados){
 //            text += u.toString()+"\n";
 //        }
 
-        tv.setText(text);
+    }
+
+    private void enviarDados(){
+
+        progressoenvio.setVisibility(View.VISIBLE);
+        Toast.makeText(DadosSalvos.this, "Enviando dados, não sair dessa tela", Toast.LENGTH_SHORT).show();
+        if (dados.size()>0){
+
+            //no banco do firebase no "galho" dados a lista sera salva
+            mDatabase.child("dados").setValue(dados).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    //evento de conclusao de envio para o feedback que foi tudo mandado
+                    Snackbar.make(tela, "Dados enviados", Snackbar.LENGTH_SHORT).show();
+                    progressoenvio.setVisibility(View.GONE);
+                    //tudo enviado trata de apagar o que ta salvo no celular
+                    AlertDialog avisodelimpeza = new AlertDialog.Builder(DadosSalvos.this)
+                            .setTitle("Aviso!").setMessage("Com os dados salvos em nuvem, os dados armazenados nesse dispositivos serão apagados")
+                            .setPositiveButton("OK", null).create();
+                    avisodelimpeza.show();
+                    Umidade.deleteAll(Umidade.class);
+                    atualizarMsg();
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    //evento de falha caso aconteça algum imprevisto
+                    Toast.makeText(DadosSalvos.this, "Algo deu errado ao enviar os dados", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }else{
+            Toast.makeText(DadosSalvos.this, "erro", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
 //=======================CONSUMO JSON=========================================
--- app/src/main/java/com/estufa/joffr/estufaapp/MainActivity.java
+++ app/src/main/java/com/estufa/joffr/estufaapp/MainActivity.java
@@ -238,7 +238,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_dados) {
             startActivity(new Intent(this, DadosSalvos.class));
         }
         return super.onOptionsItemSelected(item);
